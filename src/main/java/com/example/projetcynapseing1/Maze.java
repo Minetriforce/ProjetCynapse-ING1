90857918a86a9676e10008cfb340882aff3e95b7
Eric <eric-shao@hotmail.com>
2025-05-13 15:03:08 +0200

    update: PLEIN de commentaires dans les méthodes d'affichage de Maze

--- src/main/java/com/example/projetcynapseing1/Maze.java
+++ src/main/java/com/example/projetcynapseing1/Maze.java
@@ -66,6 +66,7 @@ public class Maze extends Graph {
      * @return number to string with padding
      */
     public static String paddingInt(int n, int padding){
+        // number of spaces to add
         int pad = padding - (int)(Math.log10((n > 1) ? n : 2) + 1);
         int padRight = pad / 2;
         int padLeft = pad - padRight;
@@ -81,50 +82,80 @@ public class Maze extends Graph {
     public String solutionToString(int[] solution) {
         String s = "";
         ArrayList<Vertex> vertices = this.getVertices();
+        // number of characters to print an int
         int padding = (int)(Math.log10(rows * columns - 1) + 1);
+        // padding must be odd
         padding += (padding % 2 == 0) ? 1 : 0;
+        // padding must > 2
         padding = (padding < 3) ? 3 : padding;
+        // absence of vertical wall
         String spaceVertical = "   ";
+        // absence of horizontal wall
         String spaceHorizontal = " ".repeat(padding);
+        // vertical wall
         String wallVertical = " | ";
+        // horizontal wall
         String wallHorizontal = "-".repeat(padding);
+        // corner
         String corner = " + ";
+        // vertical path
         String pathVertical = " * ";
+        // horizontal path
         String pathHorizontal = "*".repeat(padding);
 
+        // the maze
         for (int y = 0; y < rows; y++) {
             for (int x = 0; x < columns; x++) {
+                // id of the vertex
                 int n = y * columns + x;
                 s += Maze.paddingInt(n, padding);
+                // if not the last column
                 if (x < columns - 1) {
+                    // if vertex n + 1 neighboring
                     if (((vertices.get(n)).getNeighbors()).contains(vertices.get(n + 1))) {
+                        // if path
                         if (solution[n] == n + 1 || solution[n + 1] == n) {
                             s += pathHorizontal;
-                        } else {
+                        }
+                        // if not path
+                        else {
                             s += spaceVertical;
                         }
-                    } else {
+                    }
+                    // if there's a wall
+                    else {
                         s += wallVertical;
                     }
                 }
             }
+            // line between 2 rows
             s += "\n";
+            // if not the last row
             if (y < rows - 1) {
                 for (int x = 0; x < columns; x++) {
+                    // id of the vertex
                     int n = y * columns + x;
+                    // if vertex n + columns neighboring
                     if (((vertices.get(n)).getNeighbors()).contains(vertices.get(n + columns))) {
+                        // if path
                         if (solution[n] == n + columns || solution[n + columns] == n) {
                             s += pathVertical;
-                        } else {
+                        }
+                        // if not path
+                        else {
                             s += spaceHorizontal;
                         }
-                    } else {
+                    }
+                    // if there's a wall
+                    else {
                         s += wallHorizontal;
                     }
+                    // if not the last column
                     if (x < columns - 1) {
                         s += corner;
                     }
                 }
+                // next row
                 s += "\n";
             }
         }
@@ -140,42 +171,62 @@ public class Maze extends Graph {
         // other chars: ●;•;·;■;▀;▄;▌;▐;█;▓;▒;░;═;║;╔;╗;╚;╝;╬;┼;─;│;┌;┐;└;┘;+;=;-;|;*
         String s = "";
         List<Vertex> vertices = this.getVertices();
+        // number of characters to print an int
         int padding = (int)(Math.log10(rows * columns - 1) + 1);
+        // padding must be odd
         padding += (padding % 2 == 0) ? 1 : 0;
+        // padding must > 2
         padding = (padding < 3) ? 3 : padding;
+        // absence of vertical wall
         String spaceVertical = "   ";
+        // absence of horizontal wall
         String spaceHorizontal = " ".repeat(padding);
+        // vertical wall
         String wallVertical = " | ";
+        // horizontal wall
         String wallHorizontal = "-".repeat(padding);
+        // corner
         String corner = " + ";
 
+        // the maze
         for (int y = 0; y < rows; y++) {
             for (int x = 0; x < columns; x++) {
+                // id of the vertex
                 int n = y * columns + x;
                 s += Maze.paddingInt(n, padding);
+                // if not the last column
                 if (x < columns - 1) {
+                    // if there's not a wall
                     if (((vertices.get(n)).isNeighbor(vertices.get(n + 1)))){
                         s += spaceVertical;
                     }
+                    // if there's a wall
                     else{
                         s += wallVertical;
                     }
                 }
             }
+            // line between 2 rows
             s += "\n";
+            // if not the last row
             if (y < rows - 1) {
                 for (int x = 0; x < columns; x++) {
+                    // id of the vertex
                     int n = y * columns + x;
+                    // if there's not a wall
                     if (((vertices.get(n)).isNeighbor(vertices.get(n + columns)))){
                         s += spaceHorizontal;
                     }
+                    // if there's a wall
                     else{
                         s += wallHorizontal;
                     }
+                    // if not the last column
                     if (x < columns - 1) {
                         s += corner;
                     }
                 }
+                // next row
                 s += "\n";
             }
         }
