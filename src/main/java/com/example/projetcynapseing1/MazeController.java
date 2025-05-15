2e3c712c5fd5072ffb7b05e6872fb47ca9bbd203
Florianne <186144350+Floriannee@users.noreply.github.com>
2025-05-15 14:54:04 +0200

    test

--- src/main/java/com/example/projetcynapseing1/MazeController.java
+++ src/main/java/com/example/projetcynapseing1/MazeController.java
@@ -78,7 +78,7 @@ public class MazeController {
      */
     public void findSolution(MethodName.SolveMethodName solveMethod, Vertex start, Vertex end, MethodName.Type type,
             Double timeStamp) {
-        mazeSolver = new Solver(solveMethod,fxController);
+        mazeSolver = new Solver(solveMethod);
         // solution = mazeSolver.solveAstar(maze, start, end, type);
     }
 
